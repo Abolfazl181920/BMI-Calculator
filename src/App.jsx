e51e076644b1ee906f6cb9cbd589acd219bdfd6f
Abolfazl <abolfazlm181920@gmail.com>
2022-07-01 23:03:44 +0430

    prevent form / setStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react'
+
 const App = () => {
 
-	const calCulateBmi = (event) => {
+	const [ height, setHeight ] = useState(null)
+	const [ width, setWidth ] = useState(null)
+	const [ bmiResult, setBmiResult ] = useState(null)
+
+	const calCulateBMI = (event) => {
 		event.preventDefault()
 	}
-	
+
 	return (
 		<div className="App">
 			<form action="#" className="flex">
 				<label htmlFor="height"></label>
-				<input type="number" placeholder="Enter Height" name="height" />
 				<label htmlFor="width"></label>
-				<input type="number" placeholder="Enter Width" name="width" />
-				<button onClick={calCulateBmi}>Calculate BMI</button>
+				<input
+					value={height}
+					onChange={(event) => setHeight(event.target.value)}
+					type="number"
+					placeholder="Enter Height"
+					name="height"
+				/>
+				<input
+					value={width}
+					onChange={(event) => setWidth(event.target.value)}
+					type="number"
+					placeholder="Enter Width"
+					name="width"
+				/>
+				<button onClick={calCulateBMI}>Calculate BMI</button>
 			</form>
 		</div>
 	)
