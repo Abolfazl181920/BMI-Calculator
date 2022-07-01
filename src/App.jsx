6dff4c079eb4f276d49413b88f523a3c5c3ce56a
Abolfazl <abolfazlm181920@gmail.com>
2022-07-01 23:17:49 +0430

    delete junk className

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
 
 	return (
 		<div className="App">
-			<form action="#" className="flex">
+			<form action="#">
 				<label htmlFor="height"></label>
 				<label htmlFor="width"></label>
 
