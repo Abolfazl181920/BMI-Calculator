4cac89ab563da101df4fba4cbc1a7cff921a2825
Abolfazl <abolfazlm181920@gmail.com>
2022-07-01 23:14:03 +0430

    done calculating BMI

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
 
-	const [ height, setHeight ] = useState(null)
-	const [ width, setWidth ] = useState(null)
+	const [ height, setHeight ] = useState("")
+	const [ width, setWidth ] = useState("")
 	const [ bmiResult, setBmiResult ] = useState(null)
+	const [ status, setStatus ] = useState("")
 
 	const calCulateBMI = (event) => {
 		event.preventDefault()
+
+		let bmi = Number(width / (height / 100) ** 2).toFixed(2)
+		setBmiResult(bmi)
+
+		let bmiStatus = getStatus(bmi)
+		setStatus(bmiStatus)
+	}
+
+	const getStatus = (bmi) => {
+		if (bmi < 18.5) return "UnderWeight";
+		else if (bmi >=18.5) return "Normal";
+		else if (bmi >= 24.5 && bmi < 29.9) return "OverWeight";
+		else return "Nothing"
 	}
 
 	return (
@@ -15,6 +29,7 @@ const App = () => {
 			<form action="#" className="flex">
 				<label htmlFor="height"></label>
 				<label htmlFor="width"></label>
+
 				<input
 					value={height}
 					onChange={(event) => setHeight(event.target.value)}
@@ -30,6 +45,11 @@ const App = () => {
 					name="width"
 				/>
 				<button onClick={calCulateBMI}>Calculate BMI</button>
+
+				<>
+					<span> {bmiResult} </span>
+					<span> {status} </span>
+				</>
 			</form>
 		</div>
 	)
